1625e8978e995e785ba541e573340b72ea49d669
ashutosh21414 <ashutosh.21414@lpu.co.in>
2018-10-10 01:33:13 +0530

    sync

---
+++ src/app/login-comp/login-comp.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-comp',
+  templateUrl: './login-comp.component.html',
+  styleUrls: ['./login-comp.component.css']
+})
+export class LoginCompComponent implements OnInit {
+
+  user:any={
+    email:'',
+    password:''
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
